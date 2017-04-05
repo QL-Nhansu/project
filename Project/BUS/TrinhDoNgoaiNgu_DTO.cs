f7ff192c39cad15fcbe659ac323ea0c4d49b0c56
ngabao <nguyenthiennga.mingo@gmail.com>
2017-04-05 15:01:17 +0700

    them doi tuong TDHV, TDNN, TP

---
+++ Project/BUS/TrinhDoNgoaiNgu_DTO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public  class TrinhDoNgoaiNgu_DTO
+    {
+        private string ma;
+        private string ten;
+        private string ngonngu;
+        private string chuthich;
+
+        public string Ma
+        {
+            get
+            {
+                return ma;
+            }
+
+            set
+            {
+                ma = value;
+            }
+        }
+
+        public string Ten
+        {
+            get
+            {
+                return ten;
+            }
+
+            set
+            {
+                ten = value;
+            }
+        }
+
+        public string Ngonngu
+        {
+            get
+            {
+                return ngonngu;
+            }
+
+            set
+            {
+                ngonngu = value;
+            }
+        }
+
+        public string Chuthich
+        {
+            get
+            {
+                return chuthich;
+            }
+
+            set
+            {
+                chuthich = value;
+            }
+        }
+    }
+}
