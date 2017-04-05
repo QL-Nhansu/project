6f840188ade5309cdd42f69f77e5792d4d340625
ngabao <nguyenthiennga.mingo@gmail.com>
2017-04-05 14:34:23 +0700

    them doi tuong ChucDanh, KhenThuong,KyLuat,NhanVien

---
+++ Project/BUS/ChucDanh_DTO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class ChucDanh_DTO
+    {
+        private string ma;
+        private string ten;
+        private string chuthich;
+
+        public string Ma
+        {
+            get
+            {
+                return ma;
+            }
+
+            set
+            {
+                ma = value;
+            }
+        }
+
+        public string Ten
+        {
+            get
+            {
+                return ten;
+            }
+
+            set
+            {
+                ten = value;
+            }
+        }
+
+        public string Chuthich
+        {
+            get
+            {
+                return chuthich;
+            }
+
+            set
+            {
+                chuthich = value;
+            }
+        }
+    }
+}
